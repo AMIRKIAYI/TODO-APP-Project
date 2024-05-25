a13a7cd3c81ca5afce158b82ec30f1db9c501c23
AMIRKIAYI <amirkiayi516@gmail.com>
2024-05-25 14:34:31 +0300

    full project update

---
+++ index.js
@@ -0,0 +1,51 @@
+
+
+const inputfield = document.getElementById("input");
+const orderedList = document.getElementById("orderedList");
+
+// orderedList.addEventListener("click", function(e){
+//     e.target.remove();
+
+// })
+document.getElementById("button-addon2").addEventListener("click",function(){
+    if(inputfield.value === ""){
+        alert("Your inputfield is empty")
+    }else{
+        
+        const list = document.createElement("li")
+        list.textContent = inputfield.value
+        list.classList.add("list-group-item", "category-tag")
+        orderedList.appendChild(list)
+        
+        
+
+        
+    }
+    inputfield.value = "";
+
+})
+orderedList.addEventListener("click",function(e){
+    e.target.style.textDecoration = "line-through"
+})
+
+document.addEventListener("keydown", function(e){
+
+    if(e.key === "Enter")
+       if(inputfield.value === ""){
+        alert("Your inputfield is empty")
+       }else{
+
+        const list = document.createElement("li")
+        list.textContent = inputfield.value
+        list.classList.add("list-group-item", "category-tag")
+        orderedList.appendChild(list)
+        inputfield.value = "";
+       
+    }
+    
+    
+})
+
+
+
+
